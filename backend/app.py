d5bb7cf2b83372b77c01edb83a466b389e1eaa2f
skylucacius <skylucacius@hotmail.com>
2020-07-03 19:59:46 -0300

    backend iniciado

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+# from flask_restful import Resource, Api
+
+app = Flask(__name__)
+# api = Api(app)
+
+# class Teste(Resource):
+#     def get(self):
+#         print('Hello World')
+#         return 'teste'
+
+# api.add_resource(Teste, '/')
+
+print('testeeee')
+print('x')
+print('adsasjdkasd')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
